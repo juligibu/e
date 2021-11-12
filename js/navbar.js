950a626e7269cb6277865bbcaf722f0d43c579ae
juligibu <74247651+juligibu@users.noreply.github.com>
2021-11-12 15:01:28 -0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,7 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 50) {
+    if(window.scrollY > nav.offsetHeight + 50 && window.screen.width > 850) {
         nav.classList.add('active')
     }else {
         nav.classList.remove('active')
@@ -30,3 +30,41 @@ addEventListener('DOMContentLoaded', () => {
         })
     }
 })
+
+// CARROUSEL
+
+let indice = 1;
+muestraSlides(indice);
+
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},4000);
+function muestraSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('miSlider');
+    let barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
